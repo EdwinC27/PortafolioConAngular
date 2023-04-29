f8b10105810eba45573d6be0b14acc9142b0ba7f
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-04-29 16:26:50 -0600

    Agregar en variables los textos de la seccion de Mi pasion

--- src/app/mi-pasion/mi-pasion.component.ts
+++ src/app/mi-pasion/mi-pasion.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./mi-pasion.component.css']
 })
 export class MiPasionComponent {
+  title: String = "Mi pasión ... Ciberseguridad";
+  sub_title: String = "La ciberseguridad abarca todos los dispositivos electrónicos, por lo que es un tema relevante en todos los ámbitos.";
+  parafo: String = " Para seguir desarrollándome en este campo, estoy estudiando diferentes cursos relacionados con la seguridad informática, testing, hacking, programación, redes y otros temas. Estoy comprometido en aprender todo lo que pueda para poder aplicar mis habilidades en la protección de sistemas y redes.";
 
 }
